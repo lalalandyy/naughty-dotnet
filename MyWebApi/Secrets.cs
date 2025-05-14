e4b51f59d371195b4dc3b69f39c5d168ce42ab2f
linda <95432852+lindalacic@users.noreply.github.com>
2025-05-14 13:42:19 +0200

    Update Secrets.cs remove alles

--- MyWebApi/Secrets.cs
+++ MyWebApi/Secrets.cs
@@ -1,4 +1,3 @@
 public static class Secrets
 {
-    public static string ConnectionString => "Server=tcp:sdsoftware-server.database.windows.net,1433;Initial Catalog=sdsoftware-aikido-secret;Persist Security Info=False;User ID=sanderadmin;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 }
